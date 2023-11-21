2f5f37c9d96f98c865ef2bcffb842a01beb7b0c0
MyeongJun Go <myeongjun.ko@gmail.com>
2023-11-21 11:42:21 +0900

    created detail screen

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+import '../models/cat.dart';
+
+final List<String> replies = [
+  "Look at the Sky!",
+  "It's such a beautiful day.",
+  "See u again.",
+];
+
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({
+    super.key,
+    required this.cat,
+  });
+
+  final Cat cat;
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.cat.title),
+        ),
+        body: SafeArea(
+          child: ListView(
+            padding: const EdgeInsets.only(
+              top: 10,
+              left: 10,
+              right: 10,
+            ),
+            physics: const ClampingScrollPhysics(),
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: Image.asset(
+                  widget.cat.link,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.cat.name,
+                    style: const TextStyle(
+                        fontSize: 20.0,
+                        color: Color(
+                          0xFF777777,
+                        )),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.thumb_up_outlined,
+                        ),
+                        padding: EdgeInsets.zero,
+                      ),
+                      Text(
+                        widget.cat.likeCount.toString(),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Text("댓글 ${widget.cat.replyCount}개"),
+              ...List.generate(
+                  replies.length,
+                  (int index) => Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          children: [
+                            const Text(
+                              "익명",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 3.0),
+                            ),
+                            Text(
+                              replies[index],
+                            )
+                          ],
+                        ),
+                      )),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 10.0,
+                ),
+                child: Text(
+                    "${widget.cat.created.year}년 ${widget.cat.created.month}월 ${widget.cat.created.day}일",
+                    style: const TextStyle(
+                        color: Color(
+                      0XFFAAAAAA,
+                    ))),
+              ),
+            ],
+          ),
+        ));
+  }
+}
--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:daily_cats_app/screens/detail_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../models/cat.dart';
@@ -8,16 +9,16 @@ final List<Cat> cats = [
     name: "별님이",
     title: "오늘의 별",
     link: "assets/images/cat_00.jpg",
-    likeCount: 130,
+    likeCount: 110,
     replyCount: 3,
-    created: DateTime(2023, 11, 14, 19, 11, 53, 123),
+    created: DateTime(2023, 11, 13, 19, 11, 53, 123),
   ),
   Cat(
     id: "1",
     name: "달님이",
     title: "오늘의 달",
     link: "assets/images/cat_01.jpg",
-    likeCount: 130,
+    likeCount: 10,
     replyCount: 3,
     created: DateTime(2023, 11, 14, 19, 11, 53, 123),
   ),
@@ -26,9 +27,9 @@ final List<Cat> cats = [
     name: "햇님이",
     title: "오늘의 해",
     link: "assets/images/cat_02.jpg",
-    likeCount: 130,
+    likeCount: 230,
     replyCount: 3,
-    created: DateTime(2023, 11, 14, 19, 11, 53, 123),
+    created: DateTime(2023, 11, 15, 19, 11, 53, 123),
   ),
   Cat(
     id: "3",
@@ -37,25 +38,25 @@ final List<Cat> cats = [
     link: "assets/images/cat_03.jpg",
     likeCount: 130,
     replyCount: 3,
-    created: DateTime(2023, 11, 14, 19, 11, 53, 123),
+    created: DateTime(2023, 11, 16, 19, 11, 53, 123),
   ),
   Cat(
     id: "4",
     name: "구름이",
     title: "오늘의 구름",
     link: "assets/images/cat_04.jpg",
-    likeCount: 130,
+    likeCount: 52,
     replyCount: 3,
-    created: DateTime(2023, 11, 14, 19, 11, 53, 123),
+    created: DateTime(2023, 11, 17, 19, 11, 53, 123),
   ),
   Cat(
     id: "5",
     name: "별님이",
     title: "오늘의 별",
     link: "assets/images/cat_05.jpg",
-    likeCount: 130,
+    likeCount: 90,
     replyCount: 3,
-    created: DateTime(2023, 11, 14, 19, 11, 53, 123),
+    created: DateTime(2023, 11, 18, 19, 11, 53, 123),
   ),
 ];
 
@@ -90,7 +91,13 @@ class _ListScreenState extends State<ListScreen> {
         ),
         itemCount: cats.length,
         itemBuilder: (_, int index) => GestureDetector(
-            onTap: () {},
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(cat: cats[index]),
+                ),
+              );
+            },
             child: Image.asset(
               cats[index].link,
               fit: BoxFit.cover,
