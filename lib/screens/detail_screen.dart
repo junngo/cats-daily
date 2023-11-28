8a979da9b5bd4d6b7dcca836f998d5f56ce2816f
MyeongJun Go <myeongjun.ko@gmail.com>
2023-11-28 17:57:35 +0900

    added input form of detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -28,79 +28,115 @@ class _DetailScreenState extends State<DetailScreen> {
           title: Text(widget.cat.title),
         ),
         body: SafeArea(
-          child: ListView(
-            padding: const EdgeInsets.only(
-              top: 10,
-              left: 10,
-              right: 10,
-            ),
-            physics: const ClampingScrollPhysics(),
+          child: Stack(
             children: [
-              AspectRatio(
-                aspectRatio: 1,
-                child: Image.asset(
-                  widget.cat.link,
-                  fit: BoxFit.cover,
+              ListView(
+                padding: const EdgeInsets.only(
+                  top: 10,
+                  left: 10,
+                  right: 10,
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                physics: const ClampingScrollPhysics(),
                 children: [
-                  Text(
-                    widget.cat.name,
-                    style: const TextStyle(
-                        fontSize: 20.0,
-                        color: Color(
-                          0xFF777777,
-                        )),
+                  AspectRatio(
+                    aspectRatio: 1,
+                    child: Image.asset(
+                      widget.cat.link,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.thumb_up_outlined,
-                        ),
-                        padding: EdgeInsets.zero,
-                      ),
                       Text(
-                        widget.cat.likeCount.toString(),
+                        widget.cat.name,
+                        style: const TextStyle(
+                            fontSize: 20.0,
+                            color: Color(
+                              0xFF777777,
+                            )),
                       ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.thumb_up_outlined,
+                            ),
+                            padding: EdgeInsets.zero,
+                          ),
+                          Text(
+                            widget.cat.likeCount.toString(),
+                          ),
+                        ],
+                      )
                     ],
-                  )
+                  ),
+                  Text("댓글 ${widget.cat.replyCount}개"),
+                  ...List.generate(
+                      replies.length,
+                      (int index) => Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Row(
+                              children: [
+                                const Text(
+                                  "익명",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                const Padding(
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 3.0),
+                                ),
+                                Text(
+                                  replies[index],
+                                )
+                              ],
+                            ),
+                          )),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10.0,
+                    ),
+                    child: Text(
+                        "${widget.cat.created.year}년 ${widget.cat.created.month}월 ${widget.cat.created.day}일",
+                        style: const TextStyle(
+                            color: Color(
+                          0XFFAAAAAA,
+                        ))),
+                  ),
                 ],
               ),
-              Text("댓글 ${widget.cat.replyCount}개"),
-              ...List.generate(
-                  replies.length,
-                  (int index) => Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: Row(
-                          children: [
-                            const Text(
-                              "익명",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 3.0),
-                            ),
-                            Text(
-                              replies[index],
-                            )
-                          ],
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.only(
+                      top: 10.0,
+                      bottom: 7.0,
+                    ),
+                    color: Theme.of(context).cardColor,
+                    child: const TextField(
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(
+                          top: 5.0,
+                          bottom: 5.0,
+                          left: 10.0,
                         ),
-                      )),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 10.0,
+                        border: OutlineInputBorder(),
+                        hintText: "댓글 작성",
+                        suffixIcon: Icon(
+                          Icons.send,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-                child: Text(
-                    "${widget.cat.created.year}년 ${widget.cat.created.month}월 ${widget.cat.created.day}일",
-                    style: const TextStyle(
-                        color: Color(
-                      0XFFAAAAAA,
-                    ))),
-              ),
+              )
             ],
           ),
         ));
